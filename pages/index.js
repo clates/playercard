01dcb542d373e80805f119d7ec9768336207c5ed
clates <christopherlates@gmail.com>
2021-09-01 16:50:31 -0400

    Added two filter components. Will try to add some style to them later

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import Loading from "./Loading/Loading";
 export default function Home() {
   const [players, setPlayers] = useState([]);
   const [teams, setTeams] = useState([]);
+  const [teamsFilter, setTeamsFilter] = useState('');
+  const [playerNameFilter, setPlayerNameFilter] = useState('');
 
   useEffect(() => {
     fetchPlayerData();
@@ -44,12 +46,30 @@ export default function Home() {
       });
   };
 
+  
   return (
     <>
       {teams.length && players.length ? (
         <>
+          <div className="grid grid-cols-2 gap-y-10 gap-x-6 p-10">
+            <select onChange={x => setTeamsFilter(x.target.value)} class="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+              <option key={""} value={""}>All Teams</option>
+              {
+                teams
+                  .sort((a, b) => a.ta > b.ta ? 1 : -1 )
+                  .map(x => <option key={x.ta} value={x.ta}>{`${x.city} ${x.name}`}</option>)
+              }
+            </select>
+            <div class="relative text-gray-600">
+              <input type="search" onChange={x => setPlayerNameFilter(x.target.value.split())} placeholder="Search players..." class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-full">
+              </input>
+            </div>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 p-10">
-            {players.map((x) => (
+            {players
+            .filter(player => (!teamsFilter || player.ta === teamsFilter))
+            .filter(player => (!playerNameFilter || player.fn.toLowerCase().includes(playerNameFilter) || player.ln.toLowerCase().includes(playerNameFilter)))
+            .map((x) => (
               <PlayerCard
                 key={x.slug}
                 team={teams.filter((team) => team.ta === x.ta)[0]}
